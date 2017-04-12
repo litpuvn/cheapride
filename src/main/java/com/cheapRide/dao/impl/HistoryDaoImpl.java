8a9b6b09c3419f779d7dbb3bd29bbc58a35577e8
antonyagnes <antonyagnes28@gmail.com>
2017-04-11 20:42:34 -0500

    added author name

--- src/main/java/com/cheapRide/dao/impl/HistoryDaoImpl.java
+++ src/main/java/com/cheapRide/dao/impl/HistoryDaoImpl.java
@@ -1,5 +1,7 @@
 package com.cheapRide.dao.impl;
-
+/**
+*@author: Agnes
+*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
