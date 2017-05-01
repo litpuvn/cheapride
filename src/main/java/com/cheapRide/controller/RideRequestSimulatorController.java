141a707d3392e74db62cddeb90ec0de218801cbc
parvaneh <parvaneh.sh@gmail.com>
2017-05-01 12:06:43 -0500

    RideBooking is changed

--- src/main/java/com/cheapRide/controller/RideRequestSimulatorController.java
+++ src/main/java/com/cheapRide/controller/RideRequestSimulatorController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by pshayegh on 4/28/2017.
@@ -21,8 +22,15 @@ public class RideRequestSimulatorController {
     @Autowired
     private RideRequestSimulatorService rideRequestSimulatorService;
 
-    @RequestMapping(value = "/bookRide", method = RequestMethod.POST)
-    public ResponseEntity<RideResponseModel> requestRide(@RequestBody RideRequestModel rideRequestModel) {
+//    @RequestMapping(value = "/bookRide", method = RequestMethod.POST)
+//    public ResponseEntity<RideResponseModel> requestRide(@RequestBody RideRequestModel rideRequestModel) {
+//        ResponseEntity<RideResponseModel> responseEntity;
+//        RideResponseModel rideResponseModel= rideRequestSimulatorService.getLyftRide();
+//        responseEntity = ResponseEntity.status(HttpStatus.ACCEPTED).body(rideResponseModel);
+//        return responseEntity;
+//    }
+    @RequestMapping(value = "/bookRide", method = RequestMethod.GET)
+    public ResponseEntity<RideResponseModel> requestRide(@RequestParam String rideRequestId) {
         ResponseEntity<RideResponseModel> responseEntity;
         RideResponseModel rideResponseModel= rideRequestSimulatorService.getLyftRide();
         responseEntity = ResponseEntity.status(HttpStatus.ACCEPTED).body(rideResponseModel);
